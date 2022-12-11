a120f9da555248271a66854500fe2c7912b7c238
grsagor <grsagor08@gmail.com>
2022-12-11 22:17:18 +0600

    detail description added in the fake data

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 const projects = require('./project.json');
+const blogs = require('./blog.json');
 
 /* Middleware */
 app.use(cors());
@@ -14,8 +15,21 @@ app.get('/', (req, res) => {
     res.send('Portfolio server is running');
 });
 
+/* Projects Getting */
 app.get('/projects', (req, res) => {
     res.send(projects);
+});
+
+/* Projects Getting With ID */
+app.get('/project/:id', (req, res) => {
+    const id = req.params.id;
+    const project = projects.find( project => project._id === id);
+    res.send(project);
+});
+
+/* Blogs Getting */
+app.get('/blogs', (req, res) => {
+    res.send(blogs);
 });
 
 app.listen(port, () => {
